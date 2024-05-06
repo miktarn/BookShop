1eb9cc3630c3b9433dc577ef483cffeebfb4be0d
JadeRowan <rougesoul.m@gmail.com>
2024-05-06 08:58:41 +0300

    Implemended liquibase, soft delete and new CRUD endpoints

--- src/main/java/mik/pet/project/model/dto/NewBookRequestDto.java
+++ src/main/java/mik/pet/project/model/dto/NewBookRequestDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateBookRequestDto {
+public class NewBookRequestDto {
     private String title;
     private String author;
     private String isbn;
--- src/main/java/mik/pet/project/service/BookService.java
+++ src/main/java/mik/pet/project/service/BookService.java
@@ -2,16 +2,17 @@ package mik.pet.project.service;
 
 import java.util.List;
 import mik.pet.project.exception.EntityNotFoundException;
-import mik.pet.project.model.Book;
 import mik.pet.project.model.dto.BookDto;
-import mik.pet.project.model.dto.CreateBookRequestDto;
+import mik.pet.project.model.dto.NewBookRequestDto;
 
 public interface BookService {
-    Book save(Book book);
-
     List<BookDto> findAll();
 
-    BookDto createBook(CreateBookRequestDto createBookRequestDto);
+    BookDto createBook(NewBookRequestDto newBookRequestDto);
 
     BookDto getBookById(Long id) throws EntityNotFoundException;
+
+    BookDto updateBook(Long id, NewBookRequestDto newBookRequestDto);
+
+    void deleteById(Long id);
 }
